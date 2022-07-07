030e63b37041b7dc7cda88af79f3db0b7c6da989
Icexy Kim <kimicexy2002@gmail.com>
2022-07-08 04:47:04 +0700

    move

--- Source/Main.py
+++ Source/Main.py
@@ -13,6 +13,10 @@ black = (0, 0, 0)
 purple = (172, 172, 255)
 light_brown = (230, 204, 179)
 brown = (153, 102, 51)
+light_green = (170, 255, 0)
+green = (0, 153, 0)
+red = (255, 0, 0)
+dark_red = (179, 36, 0)
 color_list1 = [black, brown]
 default_screen = (1720, 760)
 
@@ -25,30 +29,137 @@ class Text:
         pygame.display.update()
 
 
+class Surface:
+    pass
+
+
 class Snake:
-    def __init__(self):
+    def __init__(self, game, screen):
         # draw snake with 3 circle:
-        pass
+        __w = game.board.get_width()
+        __h = game.board.get_height()
+        self.n_part = 3
+        self.r = 10
+        self.part = [(__w/10, __h/5), (__w/10 -
+                                       2*self.r, __h/5), (__w/10 -
+                                                          4*self.r, __h/5)]
+
+        self.draw(game, screen, green, self.part[0][0], self.part[0][1])
+        self.draw(game, screen, green, self.part[1][0], self.part[1][1])
+        self.draw(game, screen, green, self.part[2][0], self.part[2][1])
+        game.Update(screen)
+
+    def draw(self, game, screen, color, x=0, y=0):
+        pygame.draw.circle(game.board, color, (x, y), self.r)
+        game.Update(screen)
+
+    def move_up(self, game, screen):
+        # only when y_head <= y_next_head
+        if (self.part[0][1] <= self.part[1][1]):
+            # draw new head
+            head_pos = (self.part[0][0], self.part[0][1]-2*self.r)
+            self.part.insert(0, head_pos)
+            self.draw(game, screen, green, head_pos[0], head_pos[1])
+
+            # delete tail
+            last_pos = self.part[-1]
+            self.draw(game, screen, light_brown, last_pos[0], last_pos[1])
+            self.part.pop()
+
+        # check collide to board:
+
+    def move_down(self, game, screen):
+        # only when y_head >= y_next_head
+        if (self.part[0][1] >= self.part[1][1]):
+            # draw new head
+            head_pos = (self.part[0][0], self.part[0][1]+2*self.r)
+            self.part.insert(0, head_pos)
+            self.draw(game, screen, green, head_pos[0], head_pos[1])
+
+            # delete tail
+            last_pos = self.part[-1]
+            self.draw(game, screen, light_brown, last_pos[0], last_pos[1])
+            self.part.pop()
+
+        # check collide to board:
+
+    def move_left(self, game, screen):
+        # only when x_head <= x_next_head
+        if (self.part[0][0] <= self.part[1][0]):
+            # draw new head
+            head_pos = (self.part[0][0]-2*self.r, self.part[0][1])
+            self.part.insert(0, head_pos)
+            self.draw(game, screen, green, head_pos[0], head_pos[1])
+
+            # delete tail
+            last_pos = self.part[-1]
+            self.draw(game, screen, light_brown, last_pos[0], last_pos[1])
+            self.part.pop()
+
+        # check collide to board:
+
+    def move_right(self, game, screen):
+        # only when x_head >= x_next_head
+        if (self.part[0][0] >= self.part[1][0]):
+            # draw new head
+            head_pos = (self.part[0][0]+2*self.r, self.part[0][1])
+            self.part.insert(0, head_pos)
+            self.draw(game, screen, green, head_pos[0], head_pos[1])
+
+            # delete tail
+            last_pos = self.part[-1]
+            self.draw(game, screen, light_brown, last_pos[0], last_pos[1])
+            self.part.pop()
+
+        # check collide to board:
 
 
 class Game:
     def __init__(self, screen):
-        # create board, some main contain
+        # music, score:
         mixer.music.load('Sound/play.mp3')
         mixer.music.play()
-        __w = screen.get_width()
-        __h = screen.get_height()
-        Text(screen, 'Your score: ', __w / 30, __h / 35, 36)
-        # ve board
-        edge = pygame.draw.rect(screen, brown, (0, __h/10, __w, __h*0.9))
-        board = pygame.draw.rect(screen, light_brown, (50, __h/10+50, __w - 100, __h*0.9 -100))
+        self.__w = screen.get_width()
+        self.__h = screen.get_height()
+        Text(screen, 'Your score: ', self.__w / 30, self.__h / 35, 36)
+
+        # draw board:
+        self.edge = pygame.Surface((self.__w, self.__h*0.9))
+        self.edge.fill(brown)
+        self.board = pygame.Surface((self.__w - 100, self.__h*0.9 - 100))
+        self.board.fill(light_brown)
+        self.edge.blit(self.board, (50, self.__h/10 - 35))
+        screen.blit(self.edge, (0, self.__h/10))
         pygame.display.update()
-        sleep(10)
 
-    def Run(self):
-        # init the state of game (snake, score, food)
-        pass
+    def Update(self, screen):
+        self.edge.blit(self.board, (50, self.__h/10 - 35))
+        screen.blit(self.edge, (0, self.__h/10))
+        pygame.display.update()
 
+    def Run(self, screen):
+        # init the state of game (snake, score, food, heart)
+        # open file, load game,
+        quit_game = False
+        snake = Snake(self, screen)
+        while not quit_game:
+            clock = pygame.time.Clock()
+            for event in pygame.event.get():
+                # event: pause, restart
+                if event.type == pygame.QUIT:
+                    quit_game = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        snake.move_left(self, screen)
+                    elif event.key == pygame.K_RIGHT:
+                        snake.move_right(self, screen)
+                    elif event.key == pygame.K_UP:
+                        snake.move_up(self, screen)
+                    elif event.key == pygame.K_DOWN:
+                        snake.move_down(self, screen)
+            clock.tick(90)
+        # nhan biet phim mui ten con cham ?????
+        
 
 class Menu:
     def __init__(self):
@@ -123,7 +234,7 @@ class Menu:
         self.screen.fill(white)
         pygame.display.update()
         G = Game(self.screen)
-        G.Run()
+        G.Run(self.screen)
 
     def Resume(self):
         # khoi phuc trang thai game
@@ -155,7 +266,7 @@ def main():
             menu.Show()
             if event.type == pygame.QUIT:
                 quit_game = True
-            clock.tick(30)
+            clock.tick(90)
     pygame.quit()
     quit()
 
